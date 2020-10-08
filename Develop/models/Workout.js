8b2fe11782ed06f3ccf2841fcafd4ab4c58f6272
Mary Wood <mwoodervin@gmail.com>
2020-10-08 13:33:54 -0400

    added model files

---
+++ Develop/models/Workout.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        required: true,
+        default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+            type: String,
+            required: true
+          },
+
+        name: {
+            type: String,
+            required: true
+        },
+
+        duration: {
+            type: Number,
+            required: true
+        },
+
+        weight: Number,
+        reps: Number,
+        sets: Number
+      }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
